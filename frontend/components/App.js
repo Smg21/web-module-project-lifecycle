b337935fcdb2c82b199b56ac0779fe529f7d6493
Sabrina <Smg21022@gmail.com>
2023-03-15 15:52:18 -0600

    added base for App.js

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -4,6 +4,20 @@ const URL = 'http://localhost:9000/api/todos'
 
 export default class App extends React.Component {
   render() {
-    return null
+    return (
+      <div>
+        <div id = "error">Error: No Error Here</div>
+        <div id = "todos">
+          <h2>TODOS:</h2>
+          <div>Walk The Dog</div>
+          <div>Learn React</div>
+        </div>
+        <form id="todoform">
+          <input type="text"></input>
+          <input type="submit"></input>
+          <button>Clear Completed</button>
+        </form>
+      </div>
+    )
   }
 }
